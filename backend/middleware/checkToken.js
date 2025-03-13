81ee3dfd389543f89f2c6a918a2c4589fd02b808
Nawwar <nawwar.d92@gmail.com>
2025-03-13 11:53:37 +0100

    created UserModel, finished register, login, added jwt-token, cookie, review and updated irina's code (get and update profile) updated server.js, updated usersRouter.js, Tested Code is working

---
+++ backend/middleware/checkToken.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import createError from "http-errors";
+
+import UserModel from "../models/User.js";
+
+const checkToken = async (req, res, next) => {
+  try {
+    if (!req.cookies) {
+      throw createError(401, "No cookies found in request");
+    }
+
+    const jwtToken = req.cookies.jwtToken;
+
+    if (!jwtToken) throw createError(401, "Unauthorized request");
+
+    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+
+    const user = await UserModel.findById(decoded.id);
+
+    if (!user) throw createError(401, "User no longer exists");
+
+    req.user = user;
+    req.isAuthenticated = true;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default checkToken;
