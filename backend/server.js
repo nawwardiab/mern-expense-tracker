e8e416fe00596c912e5e73b95bf2a6c5b1df38b1
Nawwar <nawwar.d92@gmail.com>
2025-03-13 14:19:21 +0100

    added isOnboarded Boolean to the User Schema, added Errorhandlers function and imported them into server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,31 @@ import "dotenv/config";
 
 import connectDB from "./utils/database.js";
 import usersRouter from "./routes/usersRouter.js";
-// import {
-//   globalErrorHandler,
-//   routeNotFound,
-// } from "./middleware/errorHandlers.js";
+import {
+  globalErrorHandler,
+  routeNotFound,
+} from "./middleware/errorHandlers.js";
 
 await connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 6000;
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
+app.use(express.json());
 
+//? Routers
 app.use("/users", usersRouter);
 
-// app.use(routeNotFound);
-// app.use(globalErrorHandler);
+//! Error Handlers
+app.use(routeNotFound);
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(
